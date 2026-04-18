47be194210dc20d06127f547aa01f18cfe7a4b1a
Upendra Kumbham <upendra.kumbham@gmail.com>
2026-04-18 18:14:01 +0100

    Added application entry point class

---
+++ src/main/java/com/tpximpact/urlshortener/UrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.tpximpact.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class UrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
+
+}
