7eec3eeece1d6d83fe9bdfc45fc46c492df7adab
Upendra Kumbham <upendra.kumbham@gmail.com>
2026-04-18 21:04:30 +0100

    Used constructor-based dependency injection in controller to inject UrlService

--- src/main/java/com/tpximpact/urlshortener/controller/UrlController.java
+++ src/main/java/com/tpximpact/urlshortener/controller/UrlController.java
@@ -1,10 +1,15 @@
 package com.tpximpact.urlshortener.controller;
 
+import com.tpximpact.urlshortener.service.UrlService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/url")
 class UrlController {
+    private final UrlService urlService;
 
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
 }
