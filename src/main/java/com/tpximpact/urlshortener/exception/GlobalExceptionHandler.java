b162940a4c8342484e5e17ca52efdc531b971729
Upendra Kumbham <upendra.kumbham@gmail.com>
2026-04-21 22:50:47 +0100

    Added custom exception classes and global exception handlers for the application

---
+++ src/main/java/com/tpximpact/urlshortener/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.tpximpact.urlshortener.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
