76b93c4356ef87c1454cb568c8faf17662863675
Upendra Kumbham <upendra.kumbham@gmail.com>
2026-04-22 11:23:23 +0100

    Tweak util class to return proper base url with slash

--- src/main/java/com/tpximpact/urlshortener/util/UrlUtils.java
+++ src/main/java/com/tpximpact/urlshortener/util/UrlUtils.java
@@ -1,22 +1,24 @@
 package com.tpximpact.urlshortener.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.URI;
 import java.util.UUID;
 
+@Slf4j
 public class UrlUtils {
 
     public static String getBaseUrl(String fullUrl) {
         URI uri = URI.create(fullUrl);
-
         String scheme = uri.getScheme();        // http / https
         String host = uri.getHost();            // example.com
         int port = uri.getPort();               // -1 if default
 
         if (port == -1) {
-            return scheme + "://" + host;
+            return scheme + "://" + host + "/";
         }
-
-        return scheme + "://" + host + ":" + port;
+        log.info("baseUrl: {}", scheme + "://" + host + ":" + port + "/");
+        return scheme + "://" + host + ":" + port + "/";
     }
 
     public static String generateAlias() {
